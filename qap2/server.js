06641a433de87da09f62654424ca29e31ebbfe5a
NLScotty <Scott.B.Normore@gmail.com>
2024-02-12 18:27:22 -0330

    Normal Completion Finished
    Finished Normal Completion (and the last user story too)

--- qap2/server.js
+++ qap2/server.js
@@ -1,27 +1,65 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+
+const routes = require('./routes.js');
 
 global.DEBUG = true;
 
+// Code Taken from your lectures. It is an event listener that 
+// when a route event is received it emits a log to the log file
+// Currently used to keep track when routes have been accessed.
+myEmitter.on('route', (url) => {
+    const d = new Date();
+    if(DEBUG) console.log(`Route Event on: ${url} at ${d}`);
+    if(!fs.existsSync(path.join(__dirname, 'logs'))) {
+      fs.mkdirSync(path.join(__dirname, 'logs'));
+    }
+    fs.appendFile(path.join(__dirname, 'logs', 'route.log'), `Route Event on: ${url} at ${d}\n`, (error) => {
+      if(error) throw error;
+    });
+});
+
 const server = http.createServer((req, res) =>{
     if(DEBUG) console.log(req.method+", "+req.url)
+    let path = './views/';
     if(req.method === 'POST'){
 
     }else{
         switch(req.url){
             case '/':
-                console.log("root");
+                path += 'index.html';
+                myEmitter.emit('route', path);
+                routes.fetchHTMLPage(path, res);
                 break;
             case '/about':
-                console.log("about");
+                path += 'about.html';
+                myEmitter.emit('route', path);
+                routes.fetchHTMLPage(path, res);
                 break;
             case '/contact':
-                console.log("contatcs");
+                path += 'contact.html';
+                myEmitter.emit('route', path);
+                routes.fetchHTMLPage(path, res);
                 break;
             case '/products':
-                console.log("products");
+                path += 'products.html';
+                myEmitter.emit('route', path);
+                routes.fetchHTMLPage(path, res);
                 break;
             case '/subscribe':
-                console.log("subscribe");
+                path += 'subscribe.html';
+                myEmitter.emit('route', path);
+                routes.fetchHTMLPage(path, res);
+                break;
+            case '/style.css':
+                console.log(path);
+                path += 'style.css';
+                myEmitter.emit('route', path);
+                routes.styleSheet(path, res);
                 break;
             default:
                 console.log('404 Not Found');
